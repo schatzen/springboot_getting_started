3ca7237b6001475aff4b461422dd052801dea1e2
schatzen <56854068+schatzen@users.noreply.github.com>
2021-02-18 13:29:12 +0900

    # 외부 설정 2부

---
+++ src/main/java/me/test/EmilyProperties.java
@@ -0,0 +1,50 @@
+package me.test;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.time.Duration;
+
+
+@Component
+@ConfigurationProperties("me")
+@Validated
+public class EmilyProperties {
+
+    @NotEmpty
+    private String name;
+
+    private String fullname;
+
+    private int age;
+
+    // properties를 설정하지 않았을 경우 default 값으로 30초가 들어감
+    private Duration sessionTimeout = Duration.ofSeconds(30);
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
--- src/main/java/me/test/SampleRunner.java
+++ src/main/java/me/test/SampleRunner.java
@@ -1,27 +1,27 @@
 package me.test;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SampleRunner implements ApplicationRunner {
 
-    @Value("${me.name}")
-    private String name;
+//    @Value("${me.name}")
+//    private String name;
+//
+//    @Value("${me.age}")
+//    private int age;
 
-    @Value("${me.age}")
-    private int age;
+    @Autowired
+    EmilyProperties emilyProperties;
 
-   @Override
+    @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("=======================");
-        System.out.println(name);
-        System.out.println(age);
+        System.out.println(emilyProperties.getFullname());
+        System.out.println(emilyProperties.getAge());
         System.out.println("=======================");
     }
 }
